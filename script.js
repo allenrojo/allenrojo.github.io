79e3df777f3e1dd15225220d23c076b7d9850022
Allen <allenrojo31@gmail.com>
2024-04-05 10:14:58 +0800

    made layout changes for responsive design

--- script.js
+++ script.js
@@ -21,23 +21,23 @@ const video = document.getElementById('video');
 const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
-            // If the video is in the viewport, play it
+            
             video.play();
         } else {
-            // If the video is not in the viewport, pause it
+            
             video.pause();
         }
     });
-}, { threshold: 0.5 }); // Trigger when at least 50% of the video is visible
+}, { threshold: 0.3 }); 
 
-// Observe the video element
+
 observer.observe(video);
 video.addEventListener('click', () => {
     if (video.paused) {
-        // If the video is paused, play it
+
         video.play();
     } else {
-        // If the video is playing, pause it
+
         video.pause();
     }
 });
