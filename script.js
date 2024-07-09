dbfe174f0b6e1de421ee59ef798ef518121bc30f
Allen <allenrojo31@gmail.com>
2024-07-09 20:07:51 +0800

    added parallax effect

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const backgroundImages = [
-        'url(renders/render01.png)',
-        'url(renders/render06.png)',
-        'url(renders/render07.png)'
-    ];
-    const changeBackgroundButton = document.getElementById('changeBackgroundButton');
-    const container = document.querySelector('.about-container'); 
-    let currentImageIndex = 0;
-    
-    function changeBackground() {
-        currentImageIndex = (currentImageIndex + 1) % backgroundImages.length;
-        container.style.backgroundImage = backgroundImages[currentImageIndex];
-    }
-    changeBackgroundButton.addEventListener('click',changeBackground);
-    changeBackground();
-
-});
-
 const video = document.getElementById('video');
 const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
